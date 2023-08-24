7b06a7ac1839492ad45a80b3efca34563322c804
franponce <franponce98@gmail.com>
2023-08-23 21:01:19 -0300

    Route componenets

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/css/Home.css';
 
 export default function Home() {
   return (
     <div>
         <h1>Hola soy el home</h1>
+        <header>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/somos">Somos</Link>
+            </li>
+            <li>
+              <Link to="/contacto">Contacto</Link>
+            </li>
+          </ul>
+        </header>
     </div>
   )
 }
